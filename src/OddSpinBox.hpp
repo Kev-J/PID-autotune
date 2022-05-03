4d192000945c9d7caa7ce025b3b4efe3359f267a
gagik <gagik.hakobyan@boovist.com>
2022-05-03 14:29:37 +0400

    put license note on OddSpinBox.hpp

--- src/OddSpinBox.hpp
+++ src/OddSpinBox.hpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2014 Kevin JOLY
+ *
+ * Authors :    Kevin JOLY <joly.kevin25@gmail.com>
+ *
+ * This file is part of PID-autotune.
+ *
+ * PID-autotune is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PID-autotune is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PID-autotune.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <QSpinBox>
@@ -6,15 +27,12 @@
 // Thanks to ChrisW67 from the Qtproject forum: 
 // http://qt-project.org/forums/viewthread/24037
 class OddSpinBox : public QSpinBox {
-    // Q_OBJECT
 public:
     OddSpinBox(QWidget *parent = 0)
         : QSpinBox(parent)
     {
         setSingleStep(2);
     }
-
-	//virtual ~OddSpinBox() = default;
 
     protected:
     int valueFromText(const QString &text) const
